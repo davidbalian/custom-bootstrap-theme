6abe16b49cc1833790a40618b9f65b5e07a35320
davidbalian <davidbalyan9@gmail.com>
2025-06-08 20:34:10 +0300

    changed main content width

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<aside class="col-md-4 site-sidebar sticky-top sticky-offset vh-100">
+<aside class="col-md-3 site-sidebar sticky-top sticky-offset vh-100 overflow-auto">
     <!-- Read Next Widget -->
     <section class="widget read-next-widget mb-4">
         <div>
