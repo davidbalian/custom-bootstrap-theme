c4aade353d8e4261a9672cfb2314b878ec0ee2d2
davidbalian <davidbalyan9@gmail.com>
2025-06-08 19:19:44 +0300

    added bootsrap icons

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     <?php wp_head(); ?>
 </head>
 <body>
