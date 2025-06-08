d9faa4fe21ef1b4a23362d5dc2095f39d0989c62
davidbalian <davidbalyan9@gmail.com>
2025-06-08 19:29:29 +0300

    changed email inpu styles

--- footer.php
+++ footer.php
@@ -60,8 +60,8 @@
                         <h5>Sign Up for Newsletter</h5>
                         <p>Sign up for our monthly newsletter and stay informed on new services and projects. There will be some freebies along the way. We do not spam!</p>
                         <form class="d-flex footer-newsletter-form">
-                            <input type="email" class="form-control me-2" placeholder="Email Address">
-                            <button class="btn btn-dark d-flex align-items-center justify-content-center" type="submit"><i class="bi bi-arrow-right"></i></button>
+                            <input type="email" class="form-control me-0" placeholder="Email Address">
+                            <button class="btn btn-newsletter d-flex align-items-center justify-content-center" type="submit"><i class="bi bi-chevron-right"></i></button>
                         </form>
                     </div>
                 </div>
