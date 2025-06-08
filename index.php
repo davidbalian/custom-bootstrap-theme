f5344b3a1033b872aae75f27eda10c56f19632d1
davidbalian <davidbalyan9@gmail.com>
2025-06-08 18:26:49 +0300

    added grid display to posts

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
 <?php
 get_header();
+?>
 
-if ( have_posts() ) :
-    while ( have_posts() ) : the_post();
-        get_template_part( 'template-parts/content', get_post_type() );
-    endwhile;
-else :
-    ?>
-    <p>No content found.</p>
+<div class="row">
     <?php
-endif;
+    if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+            ?>
+            <div class="col-md-6">
+                <?php
+                get_template_part( 'template-parts/content', get_post_type() );
+                ?>
+            </div>
+            <?php
+        endwhile;
+    else :
+        ?>
+        <div class="col-12">
+            <p>No content found.</p>
+        </div>
+        <?php
+    endif;
+    ?>
+</div>
 
+<?php
 get_footer();
